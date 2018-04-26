2eac1725be887d2690198fa47ec8619303b4a30d
Michael Grady <michael.grady@ingentek.com>
2018-04-26 11:32:33 -0400

    building nexted components module end

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './products';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin = 2;
     showImage: boolean = false;
-    listFilter : string = '';
 
-    products: any[] = [{
+    _listFilter: string;
+    get listFilter(): string { return this._listFilter; }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter)
+            : this.products;
+    }
+
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [{
         "productId": 1,
         "productName": "Leaf Rake",
         "productCode": "GDN-0011",
@@ -32,7 +44,26 @@ export class ProductListComponent {
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     }];
 
-    toggleImage() : void {
+    constructor() {
+        this.filteredProducts = this.products;
+    }
+
+    toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    onRatingClicked(message: string): void {
+        console.log(message);
+        this.pageTitle = `Product List: ${message}`;
     }
 }
