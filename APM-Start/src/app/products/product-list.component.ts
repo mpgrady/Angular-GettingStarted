f59ad0439899f5981a2b7ab9bbe3461dd85f0d20
Michael Grady <michael.grady@ingentek.com>
2018-04-30 08:07:42 -0400

    data via http

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './products';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -12,6 +13,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string { return this._listFilter; }
@@ -23,29 +25,10 @@ export class ProductListComponent implements OnInit {
 
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [{
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }];
+    products: IProduct[];
 
-    constructor() {
-        this.filteredProducts = this.products;
+    constructor(private _productService: ProductService) {
+
     }
 
     toggleImage(): void {
@@ -54,6 +37,14 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('In OnInit');
+        this._productService.getProducts()
+            .subscribe(products => {
+            this.products = products
+                this.filteredProducts = this.products;
+            },
+                error => this.errorMessage = <any>error);
+
+
     }
 
     performFilter(filterBy: string): IProduct[] {
